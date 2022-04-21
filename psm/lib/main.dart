e14c755dc459a5797a82341a24e7c11bf0d0a32c
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-04-22 03:09:24 +0700

    added home screen

--- psm/lib/main.dart
+++ psm/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:psm/screens/home_screen.dart';
-import 'package:psm/widgets/custom_bottomappbar.dart';
-import 'widgets/custom_appbar.dart';
+import 'package:psm/screens/home/home_screen.dart';
 
 void main() {
   runApp(MyApp());
--- psm/lib/screens/home/home_screen.dart
+++ psm/lib/screens/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/custom_appbar.dart';
-import '../widgets/custom_bottomappbar.dart';
+import '../../widgets/custom_appbar.dart';
+import '../../widgets/custom_bottomappbar.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
