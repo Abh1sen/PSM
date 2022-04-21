e201b25cc84365aa852ec8e1cdc08ce741c760f7
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-04-22 02:23:58 +0700

    App bar seperated to custom widget in main

--- psm/lib/main.dart
+++ psm/lib/main.dart
@@ -21,24 +21,39 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-        title: Container(
-          color: Colors.transparent,
-          child: Text(
-            //TODO add global fonts
-            'Haseena',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
+      appBar: CustomAppBar(),
       bottomNavigationBar: BottomAppBar(),
     );
   }
+}
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      title: Container(
+        color: Colors.transparent,
+        child: Text(
+          //TODO add global fonts
+          'Haseena',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(50.0);
 }
