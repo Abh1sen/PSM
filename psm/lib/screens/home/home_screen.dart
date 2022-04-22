b9e7fe4a61907fbcf3969496f2e3863d8093c3e3
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-04-23 03:24:10 +0700

    Fixed minor design issue with Carousel

--- psm/lib/screens/home/home_screen.dart
+++ psm/lib/screens/home/home_screen.dart
@@ -2,8 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:psm/models/catagory_model.dart';
 
-import '../../widgets/custom_appbar.dart';
-import '../../widgets/custom_bottomappbar.dart';
+import '../../widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/';
@@ -36,55 +35,6 @@ class HomeScreen extends StatelessWidget {
             .map((category) => HeroCarouselCard(category: category))
             .toList(),
       )),
-    );
-  }
-}
-
-class HeroCarouselCard extends StatelessWidget {
-  final Category category;
-
-  const HeroCarouselCard({
-    required this.category,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: ClipRRect(
-          borderRadius: BorderRadius.all(Radius.circular(5.0)),
-          child: Stack(
-            children: <Widget>[
-              Image.network(category.imageUrl,
-                  fit: BoxFit.cover, width: 1000.0),
-              Positioned(
-                bottom: 0.0,
-                left: 0.0,
-                right: 0.0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color.fromARGB(200, 0, 0, 0),
-                        Color.fromARGB(0, 0, 0, 0)
-                      ],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ),
-                  ),
-                  padding:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                  child: Text(
-                    category.name,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          )),
     );
   }
 }
--- psm/lib/widgets/widgets.dart
+++ psm/lib/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'custom_appbar.dart';
 export 'custom_bottomappbar.dart';
+export 'hero_carousel_card.dart';
