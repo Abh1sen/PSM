184d978e12f0272d6c029731f7e7f5341d5dc821
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-04-23 15:01:19 +0700

    Added grid view on product catalog

--- psm/lib/screens/catalog/catalog_screen.dart
+++ psm/lib/screens/catalog/catalog_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:psm/models/catagory_model.dart';
+import 'package:psm/widgets/widgets.dart';
 
+import '../../models/models.dart';
 import '../../widgets/custom_appbar.dart';
 import '../../widgets/custom_bottomappbar.dart';
 
@@ -21,10 +23,24 @@ class CatalogScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        title: 'Catalog',
-      ),
-      bottomNavigationBar: CustomBottomAppBar(),
-    );
+        appBar: CustomAppBar(
+          title: category.name,
+        ),
+        bottomNavigationBar: CustomBottomAppBar(),
+        body: GridView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1.15,
+          ),
+          itemCount: 3,
+          itemBuilder: (BuildContext context, int index) {
+            return Center(
+              child: ProductCard(
+                product: Product.products[0],
+              ),
+            );
+          },
+        ));
   }
 }
