37227cec5dd7eeb099b6189c264e787218dacafe
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-04-27 02:22:14 +0700

    Added checkout button

--- psm/lib/screens/cart/cart_screen.dart
+++ psm/lib/screens/cart/cart_screen.dart
@@ -19,103 +19,134 @@ class CartScreen extends StatelessWidget {
       appBar: CustomAppBar(
         title: 'Cart',
       ),
-      bottomNavigationBar: CustomBottomAppBar(),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Container(
+          height: 70,
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.black),
+                onPressed: () {},
+                child: Text(
+                  'CHECKOUT',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline3!
+                      .copyWith(color: Colors.white),
+                ))
+          ]),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            SizedBox(
-              height: 5.0,
-            ),
-            CartProductCard(
-              product: Product.products[0],
-            ),
-            CartProductCard(
-              product: Product.products[1],
-            ),
-            Divider(
-              thickness: 3.0,
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'SUBTOTAL ',
-                        style: Theme.of(context).textTheme.headline3,
-                      ),
-                      Text(
-                        'Rp100000',
-                        style: Theme.of(context).textTheme.headline3,
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'DELIVERY ',
-                        style: Theme.of(context).textTheme.headline3,
-                      ),
-                      Text(
-                        'Rp80000',
-                        style: Theme.of(context).textTheme.headline3,
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Stack(
+            Column(
               children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.black.withAlpha(50),
+                SizedBox(
+                  height: 5.0,
+                ),
+                CartProductCard(
+                  product: Product.products[0],
+                ),
+                CartProductCard(
+                  product: Product.products[1],
+                ),
+                CartProductCard(
+                  product: Product.products[2],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Divider(
+                  thickness: 3.0,
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'SUBTOTAL ',
+                            style: Theme.of(context).textTheme.headline3,
+                          ),
+                          Text(
+                            'Rp100000',
+                            style: Theme.of(context).textTheme.headline3,
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'DELIVERY ',
+                            style: Theme.of(context).textTheme.headline3,
+                          ),
+                          Text(
+                            'Rp80000',
+                            style: Theme.of(context).textTheme.headline3,
+                          )
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.all(5.0),
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(13.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'TOTAL ',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline3!
-                              .copyWith(color: Colors.white),
-                        ),
-                        Text(
-                          'Rp180000',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline3!
-                              .copyWith(color: Colors.white),
-                        )
-                      ],
+                Stack(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withAlpha(50),
+                      ),
                     ),
-                  ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.all(5.0),
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(13.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'TOTAL ',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline3!
+                                  .copyWith(color: Colors.white),
+                            ),
+                            Text(
+                              'Rp180000',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline3!
+                                  .copyWith(color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
                 )
               ],
-            )
+            ),
           ],
         ),
       ),
