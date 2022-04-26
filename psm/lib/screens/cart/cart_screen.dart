a49d40feb01e7a2e844e98925f63b2a618f32086
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-04-27 01:18:42 +0700

    Added cart product card to widget folder

--- psm/lib/screens/cart/cart_screen.dart
+++ psm/lib/screens/cart/cart_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../models/models.dart';
-import '../../widgets/custom_appbar.dart';
-import '../../widgets/custom_bottomappbar.dart';
+import '../../widgets/widgets.dart';
+
 
 class CartScreen extends StatelessWidget {
   static const String routeName = '/cart';
@@ -31,66 +31,6 @@ class CartScreen extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class CartProductCard extends StatelessWidget {
-  final Product product;
-
-  const CartProductCard({
-    Key? key,
-    required this.product,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image.network(
-          product.imageUrl,
-          width: 100,
-          height: 80,
-          fit: BoxFit.cover,
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                product.name,
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              Text(
-                'Rp${product.price}',
-                style: Theme.of(context).textTheme.headline6,
-              )
-            ],
-          ),
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.add_circle),
-              onPressed: () {},
-            ),
-            Text(
-              '1',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            IconButton(
-              icon: Icon(Icons.remove_circle),
-              onPressed: () {},
-            ),
-          ],
-        )
-      ],
     );
   }
 }
