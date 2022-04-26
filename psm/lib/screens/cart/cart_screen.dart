a853c9da50e000c67a3a3c07bed71bd648e153a7
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-04-27 01:39:36 +0700

    Added price list in cart

--- psm/lib/screens/cart/cart_screen.dart
+++ psm/lib/screens/cart/cart_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../../models/models.dart';
 import '../../widgets/widgets.dart';
 
-
 class CartScreen extends StatelessWidget {
   static const String routeName = '/cart';
 
@@ -25,9 +24,98 @@ class CartScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10.0),
         child: Column(
           children: [
+            SizedBox(
+              height: 5.0,
+            ),
             CartProductCard(
               product: Product.products[0],
             ),
+            CartProductCard(
+              product: Product.products[1],
+            ),
+            Divider(
+              thickness: 3.0,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'SUBTOTAL ',
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                      Text(
+                        'Rp100000',
+                        style: Theme.of(context).textTheme.headline3,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'DELIVERY ',
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                      Text(
+                        'Rp80000',
+                        style: Theme.of(context).textTheme.headline3,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withAlpha(50),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.all(5.0),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(13.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'TOTAL ',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline3!
+                              .copyWith(color: Colors.white),
+                        ),
+                        Text(
+                          'Rp180000',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline3!
+                              .copyWith(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
