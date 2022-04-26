908aa2e4b0715f403380ce3a1a9e0d6ef1933791
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-04-27 00:40:19 +0700

    Added image to cart

--- psm/lib/screens/cart/cart_screen.dart
+++ psm/lib/screens/cart/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../models/models.dart';
 import '../../widgets/custom_appbar.dart';
 import '../../widgets/custom_bottomappbar.dart';
 
@@ -20,6 +21,39 @@ class CartScreen extends StatelessWidget {
         title: 'Cart',
       ),
       bottomNavigationBar: CustomBottomAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10.0),
+        child: Column(
+          children: [
+            CartProductCard(
+              product: Product.products[0],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CartProductCard extends StatelessWidget {
+  final Product product;
+
+  const CartProductCard({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.network(
+          product.imageUrl,
+          width: 100,
+          height: 80,
+          fit: BoxFit.cover,
+        ),
+      ],
     );
   }
 }
