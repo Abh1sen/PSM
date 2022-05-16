00f0b8d8537fc9b21892a830693638793c06a12d
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-05-16 23:49:12 +0700

    added cart bloc

--- psm/lib/models/cart_model.dart
+++ psm/lib/models/cart_model.dart
@@ -2,7 +2,8 @@ import 'package:equatable/equatable.dart';
 import 'package:psm/models/models.dart';
 
 class Cart extends Equatable {
-  Cart();
+  final List<Product> products;
+  Cart({this.products = const <Product>[]});
 
   double get subtotal =>
       products.fold(0.0, (total, current) => total + current.price);
@@ -38,40 +39,6 @@ class Cart extends Equatable {
 
   String get totalString => total(subtotal, deliveryFee).toStringAsFixed(2);
 
-  List<Product> products = [
-    Product(
-        name: 'Mukena 1',
-        category: 'Mukena',
-        imageUrl: 'https://fikashop.com/wp-content/uploads/2020/03/m034-1.jpeg',
-        price: 50000.0,
-        isRecommended: true,
-        isPopular: true),
-    Product(
-        name: 'Mukena 2',
-        category: 'Mukena',
-        imageUrl:
-            'https://s4.bukalapak.com/img/4778080082/w-1000/MUKENA_AQILLA_EXTRA_JUMBO_.jpg',
-        price: 50000.0,
-        isRecommended: true,
-        isPopular: false),
-    Product(
-        name: 'HomeDress 1',
-        category: 'HomeDress',
-        imageUrl:
-            'https://th.bing.com/th/id/OIP.bi3F4YemopeBeIO_tt1bIQHaHa?pid=ImgDet&rs=1',
-        price: 30000.0,
-        isRecommended: true,
-        isPopular: false),
-    Product(
-        name: 'HomeDress 2',
-        category: 'HomeDress',
-        imageUrl:
-            'https://loqalindonesia.com/wp-content/uploads/2020/12/DASTER-RENDA-SERUT-SLETING-1-FILEminimizer.jpg',
-        price: 30000.0,
-        isRecommended: false,
-        isPopular: true),
-  ];
-
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [products];
 }
