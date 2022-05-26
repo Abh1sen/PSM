b66794cd43b2edfcb39ca5408c1515469548867c
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-05-26 13:05:59 +0700

    Added checkout screen nav

--- psm/lib/main.dart
+++ psm/lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         title: 'Haseena',
         theme: theme(),
         onGenerateRoute: AppRouter.onGenerateRoute,
-        initialRoute: SplashScreen.routeName,
+        initialRoute: CheckoutScreen.routeName,
       ),
     );
   }
