16d6789665750dbe61afbef006ca802db12a37d7
Abhisen Muhsin Nisar <abhisenamarbusiness@gmail.com>
2022-06-01 23:44:45 +0700

    added checkout bloc

--- psm/lib/blocs/checkout/checkout_bloc.dart
+++ psm/lib/blocs/checkout/checkout_bloc.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:psm/blocs/cart/bloc/cart_bloc.dart';
 import 'package:psm/blocs/category/category_bloc.dart';
+import 'package:psm/repositories/checkout/checkout_repository.dart';
 
 import '../../models/models.dart';
 
@@ -9,9 +13,28 @@ part 'checkout_event.dart';
 part 'checkout_state.dart';
 
 class CheckoutBloc extends Bloc<CheckoutEvent, CheckoutState> {
-  CheckoutBloc() : super(CheckoutInitial()) {
-    on<CheckoutEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+  final CartBloc _cartBloc;
+  final CheckoutRepository _checkoutRepository;
+  StreamSubscription? _cartSubscription;
+  StreamSubscription? _checkoutSubscription;
+
+  CheckoutBloc({
+    required CartBloc cartBloc,
+    required CheckoutRepository checkoutRepository,
+  })  : _cartBloc = cartBloc,
+        _checkoutRepository = checkoutRepository,
+        super(cartBloc.state is CartLoaded
+            ? CheckoutLoaded(
+                products: (cartBloc.state as CartLoaded).cart.products,
+                subtotal: (cartBloc.state as CartLoaded).cart.subtotalString,
+                deliveryFee:
+                    (cartBloc.state as CartLoaded).cart.deliveryfeeString,
+                total: (cartBloc.state as CartLoaded).cart.totalString,
+              )
+            : CheckoutLoading());
+
+  @override
+  Stream<CheckoutState> mapEventToState(
+    CheckoutEvent event,
+  ) async* {}
 }
